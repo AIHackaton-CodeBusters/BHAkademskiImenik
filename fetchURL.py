369a437c7ffe60f9f2f60077d2395dd15d4b7444
Aida Zametica <azametica1@etf.unsa.ba>
2024-02-24 15:38:26 +0100

    Update on fetching id's from csv

--- fetchURL.py
+++ fetchURL.py
@@ -6,13 +6,14 @@ def read_pdf_links_from_csv(csv_file):
     with open(csv_file, 'r', newline='', encoding='utf-8') as file:
         reader = csv.DictReader(file)
         for row in reader:
+            semantic_paper_id = row.get('SemanticPaperId')
             pdf_info = row.get('openAccessPdf')
             if pdf_info:
                 try:
                     pdf_info_dict = json.loads(pdf_info)
                     pdf_url = pdf_info_dict.get('url')
                     if pdf_url:
-                        pdf_links.append(pdf_url)
+                        pdf_links.append({'SemanticPaperId': semantic_paper_id, 'url': pdf_url})
                 except json.JSONDecodeError:
                     print(f"Error decoding JSON: {pdf_info}")
     return pdf_links
