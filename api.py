99a12ddfae2fb582074a402fa2c1fef6725eb2af
Aida Zametica <azametica1@etf.unsa.ba>
2024-02-25 00:09:36 +0100

    Remove method

--- api.py
+++ api.py
@@ -3,30 +3,6 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/search', methods=['GET'])
-def search():
-    # Define the API endpoint URL
-    url = 'https://api.semanticscholar.org/graph/v1/paper/search'
-
-    # More specific query parameter
-    query = request.args.get('query')
-
-    # Directly define the API key (Reminder: Securely handle API keys in production environments)
-    api_key = 'NmKaEcYLf3N5DojI0Ajv26dc804QvjV4BwtLdFv1'  # Replace with the actual API key
-
-    # Define headers with API key
-    headers = {'x-api-key': api_key}
-
-    # Send the API request
-    response = requests.get(url, params={'query': query}, headers=headers)
-
-    # Check response status
-    if response.status_code == 200:
-        response_data = response.json()
-        return jsonify(response_data)
-    else:
-        return jsonify({'error': f"Request failed with status code {response.status_code}: {response.text}"}), response.status_code
-
 @app.route('/summarize/<paper_id>')
 def summarize(paper_id):
     response = getPaperSummary(paper_id)
